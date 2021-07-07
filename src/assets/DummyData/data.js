afa5a62caed2d5e41ac9e764dbfb4ae42c0efac3
lrdhosssein <ph.ramires@yahoo.com>
2021-07-07 17:16:49 +0430

    basic drag & drop cards - database for every chart

--- src/assets/DummyData/data.js
+++ src/assets/DummyData/data.js
@@ -5,7 +5,7 @@ export const data = [
     charts: [
       {
         id: "1251",
-        title: "نمودار حلقوی فروش ماهانه",
+        title: "سهم فروش",
         type: "Doughnut",
         backGroundColor: [
           "rgba(116, 103, 240, 1)",
@@ -14,54 +14,18 @@ export const data = [
           "rgba(0, 136, 132, 1)",
           "rgba(152, 148, 215, 1)",
         ],
-        option: {
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              labels: {
-                usePointStyle: true,
-              },
-              display: true,
-              rtl: false,
-              position: "left",
+        database: {
+          labels: ["110", "55", "275", "200", "35", "پره"],
+          data: [
+            {
+              label: "ُسهم فروش",
+              data: [36, 41, 2, 19, 0, 2],
             },
-          },
-          scales: {
-            title: {
-              display: false,
-            },
-            x: {
-              ticks: { display: false },
-              grid: {
-                display: false,
-                drawBorder: false,
-              },
-            },
-            y: {
-              ticks: { display: false },
-              grid: {
-                display: false,
-                drawBorder: false,
-              },
-              // beginAtZero: true,
-            },
-          },
+          ],
         },
-      },
-      {
-        id: "1252",
-        title: "نمودار کیکی فروش ماهانه",
-        type: "Pie",
-        backGroundColor: [
-          "rgba(116, 103, 240, 1)",
-          "rgba(255, 165, 80, 1)",
-          "rgba(234, 96, 97, 1)",
-          "rgba(0, 136, 132, 1)",
-          "rgba(152, 148, 215, 1)",
-        ],
         option: {
-          responsive: true,
           maintainAspectRatio: false,
+          cutout: "65%",
           plugins: {
             legend: {
               labels: {
@@ -98,7 +62,51 @@ export const data = [
         id: "1253",
         title: "نمودار خطی فروش ماهانه",
         type: "Line",
-        borderColor: "rgba(116, 103, 240, 1)",
+        borderColor: [
+          "rgba(116, 103, 240, 1)",
+          "rgba(255, 165, 80, 1)",
+          "rgba(234, 96, 97, 1)",
+          "rgba(0, 136, 132, 1)",
+          "rgba(152, 148, 215, 1)",
+        ],
+        database: {
+          labels: [
+            "فروردین",
+            "اردیبهشت",
+            "خرداد",
+            "تیر",
+            "مرداد",
+            // "شهریور",
+            // "مهر",
+            // "آبان",
+            // "آذر",
+            // "دی",
+            // "بهمن",
+            // "اسفند",
+          ],
+          data: [
+            {
+              label: "110",
+              data: [32, 25, 15, 1],
+            },
+            {
+              label: "550",
+              data: [25, 15, 5, 5],
+            },
+            {
+              label: "275",
+              data: [2, 1, 1, 1],
+            },
+            {
+              label: "200",
+              data: [25, 15, 10, 10],
+            },
+            {
+              label: "35",
+              data: [10, 15, 15, 5],
+            },
+          ],
+        },
         option: {
           tension: 0.2,
           maintainAspectRatio: false,
@@ -131,6 +139,44 @@ export const data = [
           "rgba(0, 136, 132, 1)",
           "rgba(152, 148, 215, 1)",
         ],
+        database: {
+          labels: [
+            "فروردین",
+            "اردیبهشت",
+            "خرداد",
+            "تیر",
+            "مرداد",
+            // "شهریور",
+            // "مهر",
+            // "آبان",
+            // "آذر",
+            // "دی",
+            // "بهمن",
+            // "اسفند",
+          ],
+          data: [
+            {
+              label: "110",
+              data: [32, 25, 15, 1],
+            },
+            {
+              label: "550",
+              data: [25, 15, 5, 5],
+            },
+            {
+              label: "275",
+              data: [2, 1, 1, 1],
+            },
+            {
+              label: "200",
+              data: [25, 15, 10, 10],
+            },
+            {
+              label: "35",
+              data: [10, 15, 15, 5],
+            },
+          ],
+        },
         options: {
           maintainAspectRatio: false,
           scales: {
@@ -139,46 +185,6 @@ export const data = [
             },
           },
         },
-      },
-    ],
-    database: [
-      {
-        labels: [
-          "فروردین",
-          "اردیبهشت",
-          "خرداد",
-          "تیر",
-          "مرداد",
-          // "شهریور",
-          // "مهر",
-          // "آبان",
-          // "آذر",
-          // "دی",
-          // "بهمن",
-          // "اسفند",
-        ],
-        data: [
-          {
-            label: "110",
-            data: [32, 25, 15, 1],
-          },
-          // {
-          //   label: "550",
-          //   data: [25, 15, 5, 5],
-          // },
-          // {
-          //   label: "275",
-          //   data: [2, 1, 1, 1],
-          // },
-          // {
-          //   label: "200",
-          //   data: [25, 15, 10, 10],
-          // },
-          // {
-          //   label: "35",
-          //   data: [10, 15, 15, 5],
-          // },
-        ],
       },
     ],
   },
--- src/container/Body/Body.jsx
+++ src/container/Body/Body.jsx
@@ -2,101 +2,80 @@ import React, { useState, useEffect } from "react";
 import "./Body.scss";
 import Card from "../../component/Card/Card";
 import { data } from "../../assets/DummyData/data";
-// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Body = (props) => {
   const [charts, setCharts] = useState([]);
-  // const dragEnd = (result) => {
-  //   const cards = [...cards];
 
-  //   const [orderedCards] = cards.splice(result.source.index, 1);
-  //   cards.splice(result.destination.index, 0, orderedCards);
+  const dragEnd = (result) => {
+    if (!result.destination) return;
 
-  //   setCards(cards);
-  // };
+    const card = [...charts];
+
+    const [orderedCard] = card.splice(result.source.index, 1);
+    card.splice(result.destination.index, 0, orderedCard);
+
+    setCharts(card);
+  };
 
   useEffect(() => {
+
     data.forEach((item) => {
       if (item.bankId === props.bankId) {
         setCharts(
           item.charts.map((ch) => {
             return {
               ...ch,
-              database: item.database[0],
             };
           })
         );
       }
     });
   }, [data]);
+  
   return (
-    <div className="cardsContainer">
-      {/* <DragDropContext onDragEnd={dragEnd}>
-        <Droppable droppableId="cardsSequence" direction="vertical" type="row">
+    <div>
+      <DragDropContext onDragEnd={dragEnd}>
+        <Droppable droppableId="cardsSequence" direction="horizontal">
           {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Draggable draggableId="card1" key="card1" index="1">
-                {(provided) => (
-                  <Card
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    title={stringFa.card_title[1]}
-                    chartType="Line"
-                  />
-                )}
-              </Draggable>
-              <Draggable draggableId="card2" key="card2" index="2">
-                {(provided) => (
-                  <Card
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    title={stringFa.card_title[3]}
-                    chartType="Doughnut"
-                  />
-                )}
-              </Draggable>
-              <Draggable draggableId="card3" key="card3" index="3">
-                {(provided) => (
-                  <Card
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    title={stringFa.card_title[0]}
-                    chartType="Bar"
-                  />
-                )}
-              </Draggable>
-              <Draggable draggableId="card4" key="card4" index="4">
-                {(provided) => (
-                  <Card
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    title={stringFa.card_title[2]}
-                    chartType="Radar"
-                  />
-                )}
-              </Draggable>
+            <div
+              className="cardsContainer"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {charts
+                ? charts.map((item, index) => (
+                    <Draggable
+                      draggableId={`draggable${item.id}`}
+                      key={`draggable${item.id}`}
+                      index={index}
+                      isDragDisabled={false}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <Card
+                            key={`${item.id}`}
+                            title={item.title}
+                            chartType={item.type}
+                            backGroundColor={item.backGroundColor}
+                            borderColor={item.borderColor}
+                            database={item.database}
+                            option={item.option}
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))
+                : null}
               {provided.placeholder}
             </div>
           )}
         </Droppable>
-      </DragDropContext> */}
-      {charts
-        ? charts.map((item) => (
-            <Card
-              title={item.title}
-              chartType={item.type}
-              backGroundColor={item.backGroundColor}
-              borderColor={item.borderColor}              
-              option={item.option}
-              labels={item.labels}
-              database={item.database}
-            />
-          ))
-        : null}
+      </DragDropContext>
     </div>
   );
 };
