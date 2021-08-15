406e44a46adde8e852a3d483f7277aa1471adc8e
lrdhosssein <ph.ramires@yahoo.com>
2021-08-15 20:05:18 +0430

    completed step benchmark

---
+++ src/component/Steps/BenchmarkStep/BenchmarkLine.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SettingsColor from "./SettingsColor.jsx";
+import { MdCancel } from "react-icons/md";
+
+const BenchmarkLine = (props) => {
+  return (
+    <div className="item-list-item">
+      <div className="settings-field-component item-list-item-field">
+        <div className="settings-field-component benchmark-line-value">
+          <div className="settings-field-title-wrapper">
+            <div className="settings-field-title">
+              <div>Benchmark Line</div>
+            </div>
+          </div>
+          <input type="text" placeholder="Number Value" className="input" />
+        </div>
+        <div className="settings-field-component benchmark-line-label">
+          <input
+            type="text"
+            placeholder="Benchmark Line Label"
+            className="input"
+          />
+        </div>
+        <div className="settings-field-component">
+          <SettingsColor />
+        </div>
+      </div>
+      {props.index !== "0" && (
+        <div className="item-list-item-actions">
+          <button
+            className="sijam-style-button"
+            onClick={() => props.onRemove(props.index)}
+          >
+            <MdCancel />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BenchmarkLine;
--- src/component/Steps/BenchmarkStep/BenchmarkStep.jsx
+++ src/component/Steps/BenchmarkStep/BenchmarkStep.jsx
@@ -1,41 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BenchmarkStep.scss";
-import SettingsColor from "./SettingsColor.jsx";
+import BenchmarkLine from "./BenchmarkLine.jsx";
 
 const BenchmarkStep = () => {
+  let onRemoveHandler;
+  const [benchmarkLines, setBenchmarkLines] = useState([
+    <BenchmarkLine onRemove={onRemoveHandler} index="0" />,
+  ]);
+  onRemoveHandler = (index) => {
+    let newArray = [...benchmarkLines];
+    if (index !== -1) {
+      newArray.splice(index, 1);
+      setBenchmarkLines(newArray);
+    }
+  };
+
   return (
     <div className="settings-field-container">
       <div className="settings-multiple-wrapper">
         <div className="settings-multiple-item-list">
-          <div className="item-list-item">
-            <div className="settings-field-component item-list-item-field">
-              <div className="settings-field-component benchmark-line-value">
-                <div className="settings-field-title-wrapper">
-                  <div className="settings-field-title">
-                    <div>Benchmark Line</div>
-                  </div>
-                </div>
-                <input
-                  type="text"
-                  placeholder="Number Value"
-                  className="input"
-                />
-              </div>
-              <div className="settings-field-component benchmark-line-label">
-                <input
-                  type="text"
-                  placeholder="Benchmark Line Label"
-                  className="input"
-                />
-              </div>
-              <div className="settings-field-component">
-                <SettingsColor />
-              </div>
-            </div>
-            <div className="item-list-item-actions"></div>
-          </div>
+          {benchmarkLines.map((item) => item)}
         </div>
-        <div className="settings-multiple-item-footer"></div>
+        <div className="settings-multiple-item-footer">
+          <button
+            className="sijam-style-button"
+            onClick={() =>
+              setBenchmarkLines([
+                ...benchmarkLines,
+                <BenchmarkLine
+                  onRemove={onRemoveHandler}
+                  index={benchmarkLines.length}
+                />,
+              ])
+            }
+          >
+            اضافه کردن خط معیار +
+          </button>
+        </div>
       </div>
     </div>
   );
