a9f651b8232cffe994fdc0c06a434b509f9fc088
lrdhosssein <ph.ramires@yahoo.com>
2022-03-02 18:43:18 +0330

    changed custom select component ui

--- src/component/UI/CustomSelect/CustomSelect.jsx
+++ src/component/UI/CustomSelect/CustomSelect.jsx
@@ -1,22 +1,49 @@
 import React from 'react'
 import './CustomSelect.scss'
+import { useTheme } from '../../../styles/ThemeProvider';
+
 const CustomSelect = (props) => {
+    const themeState = useTheme();
+    const theme = themeState.computedTheme;
+
     return (
         <div className='custom-select-container' style={{ ...props.style }}>
             {
                 props.title &&
-                <p>{props.title}</p>
+                <p style={{ ...props.titleStyle }}>{props.title}</p>
             }
-            <select style={{...props.selectStyle}} value={props.selectedItem} onChange={props.onSelectChangeHandler}>
+            <select
+                style={{
+                    background: themeState.isDark
+                        ? theme.surface_1dp
+                        : theme.surface,
+                    color: theme.on_background,
+                    borderColor: theme.darken_border_color,
+                    ...props.selectStyle
+                }}
+                value={props.selectedItem}
+                onChange={props.onSelectChangeHandler}>
                 {props.items && props.items.map((option) => (
                     props.path ?
                         <option key={option[props.path][props.keyField]}
-                            value={option[props.path][props.valueField]}>
+                            value={option[props.path][props.valueField]}
+                            style={{
+                                backgroundColor: themeState.isDark
+                                    ? "black"
+                                    : "white",
+                                color: theme.on_background,
+                            }}>
                             {option[props.path][props.valueField]}
                         </option>
                         :
                         <option key={option[props.keyField]}
-                            value={option[props.valueField]}>
+                            value={option[props.valueField]}
+                            style={{
+                                backgroundColor: themeState.isDark
+                                    ? "black"
+                                    : "white",
+                                color: theme.on_background,
+                            }}>
                             {option[props.valueField]}
                         </option>
 
