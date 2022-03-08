99aa1a5229b556742ef953e092f8dd686a5e147c
lrdhosssein <ph.ramires@yahoo.com>
2022-03-08 17:18:28 +0330

    backdrop dynamic color

--- src/component/UI/Backdrop/Backdrop.jsx
+++ src/component/UI/Backdrop/Backdrop.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { animated, useSpring } from "react-spring";
+import { useTheme } from "../../../styles/ThemeProvider";
 
 import classes from "./Backdrop.module.css";
 
 const Backdrop = (props) => {
+  const themeState = useTheme();
   const spring = useSpring({
+    backgroundColor: themeState.isDark ? "rgba(150, 150, 150, 0.2)" : "rgba(0, 0, 0, 0.2)",
     opacity: props.show ? 1 : 0,
     pointerEvents: props.show ? "auto" : "none",
     from: { opacity: 0, pointerEvents: "none" },
