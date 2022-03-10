e4c7a77b507bdd67fb94648d6db54e34560fe7c6
lrdhosssein <ph.ramires@yahoo.com>
2022-03-10 18:08:27 +0330

    users list in chart access

--- src/container/Body/BodySetting/BodyContentUser/BodyContentUserAccessChart/BodyContentUserAccessChart.jsx
+++ src/container/Body/BodySetting/BodyContentUser/BodyContentUserAccessChart/BodyContentUserAccessChart.jsx
@@ -4,11 +4,17 @@ import { useTheme } from '../../../../../styles/ThemeProvider';
 import { IoIosSearch } from "react-icons/io";
 import Input from '../../../../../component/UI/Input/Input';
 import { stringFa } from '../../../../../assets/strings/stringFaCollection';
+import { useSelector } from 'react-redux';
+import UserItem from './UserItem/UserItem';
 
 const BodyContentUserAccessChart = () => {
     const themeState = useTheme();
     const theme = themeState.computedTheme;
     const [loading, setLoading] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const holdingDetail = useSelector((state) => state.holdingDetail);
+    const userId = useSelector((state) => state.auth);
 
     return (
         <div className="user-access-chart-container">
@@ -27,7 +33,27 @@ const BodyContentUserAccessChart = () => {
                             <IoIosSearch size="1.5em" color={theme.darken_border_color} />
                         </div>
                     </div>
+                    <div className="users-list-container">
+                        {holdingDetail.employees &&
+                            holdingDetail.employees.length > 0 &&
+                            holdingDetail.employees.map((employee) => (
+                                <UserItem
+                                    key={employee.user._id}
+                                    data={employee}
+                                    selected={selectedUser}
+                                    setSelected={setSelectedUser}
+                                />
+                            ))}
+                    </div>
                 </div>
+                {selectedUser && <div className="chart-access">
+                    <div className="selected-user-access-title">
+                        دسترسی های
+                        <p className="selected-user"
+                            style={{ color: theme.primary }}
+                        >{selectedUser.username}</p>
+                    </div>
+                </div>}
             </div>
         </div>
     )
