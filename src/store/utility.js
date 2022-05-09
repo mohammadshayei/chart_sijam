aa094ca39e73ded97a8cce14f8b0c2e532235d0c
mohammadshayei <mmdshayei78@gmail.com>
2022-05-09 11:47:36 +0430

    not in  filter

--- src/store/utility.js
+++ src/store/utility.js
@@ -1,23 +1,25 @@
 export const updateObject = (oldObject, updatedProperties) => {
   return {
     ...oldObject,
-    ...updatedProperties
-  }
-}
+    ...updatedProperties,
+  };
+};
 
 export const filterData = (data, filterRules) => {
-  let filteredData = []
-  data.forEach(record => {
-    let valid = filterRules.operator === 'یا' ? false : true;
-    filterRules.fields.forEach(rule => {
+  let filteredData = [];
+  data.forEach((record) => {
+    let valid = filterRules.operator === "یا" ? false : true;
+    filterRules.fields.forEach((rule) => {
       let tempValid;
-      if (rule.content.value === "")
-        tempValid = true
+      if (rule.content.value === "") tempValid = true;
       else
-        tempValid = rule.content.not ? record[rule.name].data !== rule.content.value : record[rule.name].data.localeCompare(rule.content.value) === 0
-      valid = filterRules.operator === 'یا' ? (valid || tempValid) : (valid && tempValid);
+        tempValid = rule.content.not
+          ? record[rule.name].data.localeCompare(rule.content.value) !== 0
+          : record[rule.name].data.localeCompare(rule.content.value) === 0;
+      valid =
+        filterRules.operator === "یا" ? valid || tempValid : valid && tempValid;
     });
-    if (valid) filteredData.push(record)
-  })
-  return filteredData
-}
+    if (valid) filteredData.push(record);
+  });
+  return filteredData;
+};
